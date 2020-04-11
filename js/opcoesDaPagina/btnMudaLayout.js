f6094c1e24542dfeb5b04ffb941b66e590fef4cb
Mario Souto <soutomarios@gmail.com>
2020-04-11 13:52:14 -0300

    Update exercicio 3

--- js/opcoesDaPagina/btnMudaLayout.js
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -1,15 +1,41 @@
 
 
+// Pegar/Achar o elemento no html
 const btnMudaLayout = document.querySelector('#btnMudaLayout');
 
 // Organizar essas lógicas
 function mudaTexto() {
-  if(btnMudaLayout.textContent.trim() == 'Linhas') {
+  if(btnMudaLayout.textContent.trim() === 'Linhas') {
     btnMudaLayout.textContent = 'Colunas';
   } else {
     btnMudaLayout.textContent = 'Linhas';
   }
 }
+
+function mudaLayout() {
+  const mural = document.querySelector('.mural');
+  console.log('condicional do mudaLayout', mural.classList.contains('mural--linha'));
+  mural.classList.toggle('mural--linha');
+  // if(mural.classList.contains('mural--linha')) {
+  //   mural.classList.remove('mural--linha');
+  // } else {
+  //   mural.classList.add('mural--linha');
+  // }
+}
+
+btnMudaLayout.addEventListener('click', mudaTexto)
+btnMudaLayout.addEventListener('click', mudaLayout)
+
+// toggle
+
 // Acontece uma ação - Executa uma função ao final disso
 // callback - chamada de retorno
-btnMudaLayout.onclick = mudaTexto;
+
+// function mudaTextoELayout() {
+//   mudaTexto()
+//   mudaLayout()
+// }
+// btnMudaLayout.onclick = mudaTextoELayout;
+
+
+// adicionaEscutadorDeEventosDoUsuarioOuNaoNecessariamente
