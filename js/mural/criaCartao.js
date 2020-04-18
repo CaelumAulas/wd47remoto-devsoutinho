0d2bb2fc06be61009442b0bc9759e0026649c06a
Mario Souto <soutomarios@gmail.com>
2020-04-18 16:54:10 -0300

    Colocando o contador

--- js/mural/criaCartao.js
+++ js/mural/criaCartao.js
@@ -1,5 +1,9 @@
 // Não vamos colocar a IIFE
 function criaHTMLDoCartao(conteudo) {
+  // Como aumentar o ID do contador baseado nos cards da tela
+  // Conceito do Delegate
+  // Sincronização com Servidor externo
+  const contador = 0;
 
   // Copiem o HTML das Opções do cartão e coloqiuem nessa variável
   const opcoesDoCartao = `
@@ -8,30 +12,30 @@ function criaHTMLDoCartao(conteudo) {
         <svg><use xlink:href="#iconeRemover"></use></svg>
       </button>
 
-      <input type="radio" name="corDoCartao1" value="#EBEF40" id="corPadrão-cartao1" class="opcoesDoCartao-radioTipo" checked>
-      <label for="corPadrão-cartao1" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
+      <input type="radio" name="corDoCartao${contador}" value="#EBEF40" id="corPadrão-cartao${contador}" class="opcoesDoCartao-radioTipo" checked>
+      <label for="corPadrão-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
         Padrão
       </label>
 
-      <input type="radio" name="corDoCartao1" value="#F05450" id="corImportante-cartao1" class="opcoesDoCartao-radioTipo">
-      <label for="corImportante-cartao1" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
+      <input type="radio" name="corDoCartao${contador}" value="#F05450" id="corImportante-cartao${contador}" class="opcoesDoCartao-radioTipo">
+      <label for="corImportante-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
         Importante
       </label>
 
-      <input type="radio" name="corDoCartao1" value="#92C4EC" id="corTarefa-cartao1" class="opcoesDoCartao-radioTipo">
-      <label for="corTarefa-cartao1" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
+      <input type="radio" name="corDoCartao${contador}" value="#92C4EC" id="corTarefa-cartao${contador}" class="opcoesDoCartao-radioTipo">
+      <label for="corTarefa-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
         Tarefa
       </label>
 
-      <input type="radio" name="corDoCartao1" value="#76EF40" id="corInspiração-cartao1" class="opcoesDoCartao-radioTipo">
-      <label for="corInspiração-cartao1" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
+      <input type="radio" name="corDoCartao${contador}" value="#76EF40" id="corInspiração-cartao${contador}" class="opcoesDoCartao-radioTipo">
+      <label for="corInspiração-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
         Inspiração
       </label>
     </div>
   `;
 
   const cartao = `
-  <article id="cartao_1" class="cartao" tabindex="0">
+  <article id="cartao_${contador}" class="cartao" tabindex="0">
     ${opcoesDoCartao}
     <p class="cartao-conteudo" contenteditable tabindex="0">
        ${conteudo}
