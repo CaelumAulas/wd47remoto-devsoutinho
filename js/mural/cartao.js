cb4e083e1df030591bbe5a4ce797c8085be954b4
Mario Souto <soutomarios@gmail.com>
2020-04-18 11:10:40 -0300

    Ajuste do background

--- js/mural/cartao.js
+++ js/mural/cartao.js
@@ -10,5 +10,26 @@
     cartao.addEventListener('focusout', function() {
       cartao.classList.remove('cartao--focado');
     })
+
+
+    // [Capítulo 15]
+
+    // [Design Pattern: Delegate]
+    // É possível saber se um elemento filho de outro foi clicado
+    // Lembra do efeito de jogar pedrinha na agua
+    cartao.addEventListener('click', function(infosDoEvento) {
+      const alvoDoEvento = infosDoEvento.target;
+      console.log('[alvoDoEvento]', alvoDoEvento);
+      const corQueVamosAplicar = alvoDoEvento.value
+      console.log('[corQueVamosAplicar]', corQueVamosAplicar);
+
+      if(Boolean(corQueVamosAplicar)) { // O if SEMPRE converte o nosso valor
+        // para "true" ou "false"
+        console.log('Cor que vai ser aplicada', corQueVamosAplicar);
+        // Só aplicamos no cartão
+        cartao.style.background = corQueVamosAplicar;
+      }
+    })
+
   }
 })()
