7a58ba6a322e063106c5fdebd9441f6241df5f20
Mario Souto <soutomarios@gmail.com>
2020-04-18 11:57:42 -0300

    Comitao da massa

--- js/mural/cartao.js
+++ js/mural/cartao.js
@@ -1,13 +1,13 @@
 // IIFE - Módulo/Arquivo novo
 // - Garantir que não vamos ter conflito de variável
-;(function() {
+; (function () {
   const cartoes = document.querySelectorAll('.cartao');
 
-  for(const cartao of cartoes) { // ES6 "for of"
-    cartao.addEventListener('focusin', function() {
+  for (const cartao of cartoes) { // ES6 "for of"
+    cartao.addEventListener('focusin', function () {
       cartao.classList.add('cartao--focado');
     })
-    cartao.addEventListener('focusout', function() {
+    cartao.addEventListener('focusout', function () {
       cartao.classList.remove('cartao--focado');
     })
 
@@ -16,20 +16,42 @@
 
     // [Design Pattern: Delegate]
     // É possível saber se um elemento filho de outro foi clicado
-    // Lembra do efeito de jogar pedrinha na agua
+    // Lembra do efeito de jogar pedrinha na agua    
+    // cartao.addEventListener('click', function (infosDoEvento) {
+    //   const alvoDoEvento = infosDoEvento.target;
+    //   const corQueVamosAplicar = alvoDoEvento.value
+    //   if (corQueVamosAplicar) { // O if SEMPRE converte o nosso valor
+    //     cartao.style.background = corQueVamosAplicar;
+    //   }
+    // })
+
+    // [Testa o Change]
+    // cartao.addEventListener("change", function (event) {
+    //   const elementoSelecionado = event.target
+    //   const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+    //   if (isRadioTipo) {
+    //     cartao.style.backgroundColor = elementoSelecionado.value
+    //   }
+    // })
+    // Tenta fazer o código do remove aqui ao invés de fazer no outro arquivo
+    // - ./projeto-wd47/js/mural/opcoesDoCartao-remove.js
+    // Checa se estamos clicando no botão de remover e faz o removeCartao
+
     cartao.addEventListener('click', function(infosDoEvento) {
       const alvoDoEvento = infosDoEvento.target;
-      console.log('[alvoDoEvento]', alvoDoEvento);
-      const corQueVamosAplicar = alvoDoEvento.value
-      console.log('[corQueVamosAplicar]', corQueVamosAplicar);
+      const isBtnRemove = alvoDoEvento.classList.contains('opcoesDoCartao-remove');
+      const isRadioTipo = alvoDoEvento.classList.contains('opcoesDoCartao-radioTipo');
 
-      if(Boolean(corQueVamosAplicar)) { // O if SEMPRE converte o nosso valor
-        // para "true" ou "false"
-        console.log('Cor que vai ser aplicada', corQueVamosAplicar);
-        // Só aplicamos no cartão
-        cartao.style.background = corQueVamosAplicar;
+      if(isBtnRemove) {
+        cartao.classList.add('cartao--some');
+        cartao.addEventListener('transitionend', function() {
+          cartao.remove();
+        });
+      }
+
+      if(isRadioTipo) {
+        cartao.style.background = alvoDoEvento.value
       }
     })
-
   }
 })()
--- js/mural/opcoesDoCartao-remove.js
+++ js/mural/opcoesDoCartao-remove.js
@@ -1,32 +1,32 @@
-// IIFE
-// Imediatly Invocked Function by Expression
-// Função que executa automagicamente
-// Module Pattern
-;(function () { // Todo arquivo rodando no Node tem isso em volta dele
-  // As variáveis por padrão são globais
-  // Variáveis dentro de funções são "locais", pertecem a função que elas foram criadas
+// // IIFE
+// // Imediatly Invocked Function by Expression
+// // Função que executa automagicamente
+// // Module Pattern
+// ;(function () { // Todo arquivo rodando no Node tem isso em volta dele
+//   // As variáveis por padrão são globais
+//   // Variáveis dentro de funções são "locais", pertecem a função que elas foram criadas
 
-  // - selecionar o primeiro botão de apagar
-  const btnsRemove = document.querySelectorAll('.opcoesDoCartao-remove');
+//   // - selecionar o primeiro botão de apagar
+//   const btnsRemove = document.querySelectorAll('.opcoesDoCartao-remove');
 
-  // - criar um addEventListener de click || - criar a função que apaga
+//   // - criar um addEventListener de click || - criar a função que apaga
   
-  // Statements também tem escopo (if, while, for...)
-  for(let indice = 0; indice < btnsRemove.length; indice++) {
-    console.log('[indice]', indice)
-    btnsRemove[indice].addEventListener('click', function () {
-      //   - selecionar o cartão lá dentro para poder apagar
-      console.log('[indice]', indice)
-      console.log('btnsRemove[indice]', btnsRemove[indice]);
-      const cartao = btnsRemove[indice].parentNode.parentNode;
-      cartao.classList.add('cartao--some');
-      // Fazendo o botão ripple do Google com isso aqui: https://www.youtube.com/watch?v=YyoSLwngaKs
-      cartao.addEventListener('transitionend', function() {
-        cartao.remove();
-      });
-    });
-  }
+//   // Statements também tem escopo (if, while, for...)
+//   for(let indice = 0; indice < btnsRemove.length; indice++) {
+//     console.log('[indice]', indice)
+//     btnsRemove[indice].addEventListener('click', function () {
+//       //   - selecionar o cartão lá dentro para poder apagar
+//       console.log('[indice]', indice)
+//       console.log('btnsRemove[indice]', btnsRemove[indice]);
+//       const cartao = btnsRemove[indice].parentNode.parentNode;
+//       cartao.classList.add('cartao--some');
+//       // Fazendo o botão ripple do Google com isso aqui: https://www.youtube.com/watch?v=YyoSLwngaKs
+//       cartao.addEventListener('transitionend', function() {
+//         cartao.remove();
+//       });
+//     });
+//   }
   
-  // forEach
+//   // forEach
 
-})();
+// })();
