804f872bcdf22d911128f309e5d1081b475a27f0
Mario Souto <soutomarios@gmail.com>
2020-05-02 10:08:13 -0300

    Upload código manhã

--- src/js/opcoesDaPagina/btnSync.js
+++ src/js/opcoesDaPagina/btnSync.js
@@ -18,6 +18,52 @@
     })
   
     console.log('[cartoesProServer]', cartoesProServer);
+
+
+    // Enviar os cartoesProServer pro server de fato
+
+    // GET, [POST], DELETE...
+    const contratoDoServidor = {
+      usuario: 'mario.souto@caelum.com.br',
+      cartoes: cartoesProServer
+    };
+
+    $btnSync.classList.add("botaoSync--esperando");
+    $btnSync.classList.remove("botaoSync--sincronizado");
+    fetch('https://ceep.herokuapp.com/cartoes/salvar', {
+      method: 'POST',
+      body: JSON.stringify(contratoDoServidor),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    // Pra cada chamada que der certo e for encadeada em sequencia
+    .then(function(respostaDoServer) {
+      console.log('Request', respostaDoServer);
+      if(respostaDoServer.ok) {
+        $btnSync.classList.add("botaoSync--sincronizado");
+        return;
+      }
+
+      throw new Error('Deu algum problema');
+    })
+    // Pra quando der errado
+    .catch(() => {
+      console.log('Deu algum erro?');
+      $btnSync.classList.add("botaoSync--deuRuim");
+    })
+    // Ao final de tudo
+    .finally(() => {
+      $btnSync.classList.remove("botaoSync--esperando");
+    })
+
+
+    // REQUISIÇÃO/REQUEST
+    /*
+      - Headers (quem envia e quem recebe)
+      - PAYLOAD === Body
+    */
+
   });
 
   
